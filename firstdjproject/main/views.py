b590cb11d3ca792492a81da2d75e4f8d8d64147f
oblivion4us <oblivion4us@icloud.com>
2024-11-20 13:53:33 +0200

    update files settings.py, main.urls.py and main.views.py

--- firstdjproject/main/views.py
+++ firstdjproject/main/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 def index(responce):
     return HttpResponse("<h1>Привет!</h1>")
 
+def data(responce):
+    return HttpResponse("<h1>Страница data</h1>")
+
+def test(responce):
+    return HttpResponse("<h1>Страница test</h1>")
